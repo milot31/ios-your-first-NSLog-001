1302c4e000daeba79df4725907f1d8a7dbb9fc7c
milot31 <milot.phil@gmail.com>
2015-07-18 10:23:27 -0400

    Updated code

--- your-first-NSLog/FISAppDelegate.m
+++ your-first-NSLog/FISAppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
+    // HEY!
+    
+    // write your code here! :D
     NSLog(@"Hello, World!");
     
     return YES; // except don't touch this line
